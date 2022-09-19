044a3c05fc14261c2e4dd98b9f2109990c5840cb
Rolf Bjarne Kvinge <rolf@xamarin.com>
2022-09-19 15:32:25 +0200

    Revert "Auto-format source code"
    This reverts commit d9e19d408d4623d027b097cb6bd4c4b1688dcc32.

--- test-app/Program.cs
+++ test-app/Program.cs
@@ -6,17 +6,16 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Resources;
 
-class gdb
-{
-    static void Main(string[] args)
-    {
-        Thread.CurrentThread.CurrentCulture = new CultureInfo("es-ES");
-        Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+class gdb {
+	static void Main (string [] args)
+	{
+		Thread.CurrentThread.CurrentCulture = new CultureInfo ("es-ES");
+		Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
 
-        ResourceManager temp = new ResourceManager("ConsoleApplication1.Resources", typeof(gdb).Assembly);
+		ResourceManager temp = new ResourceManager("ConsoleApplication1.Resources", typeof(gdb).Assembly);
 
-        string res = temp.GetString("String1");
-        //string res = ConsoleApplication1.Properties.Resources.String1;
-        Console.WriteLine(res);
-    }
+		string res = temp.GetString ("String1");
+		//string res = ConsoleApplication1.Properties.Resources.String1;
+		Console.WriteLine (res);
+	}
 }
